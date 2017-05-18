89e9d469fe9848dc2aa1e1ead685a5e090ce058c
Brian McGowan <mcgowan.b@gmail.com>
2017-05-18 11:46:54 +0100

    Modifyng files for revision

--- api-db/server.js
+++ api-db/server.js
@@ -2,49 +2,48 @@ var express = require('express');
 var path = require('path');
 var mysql = require('mysql');
 var app = express();
-const cors = require('cors');
+const cors = require('cors')
 
 const corsOptions = {
-    origin: "http://localhost:4200"
+    origin: 'http://localhost:4200'
 }
 
-app.use(cors(corsOptions));
+app.use(cors(corsOptions))
 
 var connection = mysql.createConnection({
-    host: 'college-angular-db.cucaivb9nhjv.eu-west-1.rds.amazonaws.com',
+    host: 'mysql-api-test.c2exmftaihrq.eu-west-1.rds.amazonaws.com',
     user: 'root',
     password: 'password'
 });
 
 connection.connect(function (error) {
     if (!!error) {
-        console.log('Error ' + error);
+        console.log('Error' + error);
     }
-    else
+    else {
         console.log('Connected');
-});
+    }
 
-connection.query('USE api_test');
+})
+connection.query('USE API_Test');
 app.set('port', 3000);
 app.set('view engine', 'jade');
-
 var programmeName;
 
 app.get('/', function (req, res) {
-    connection.query('SELECT CourseName FROM Course WHERE id = 2',
-        function (err, rows) {
-            if (!!err)
-                console.log("Error in the query");
+    connection.query('select ProgrammeName from Programme where idProgramme=1', function (err, rows) {
+        if (!!err) {
+            console.log('Error in the query');
+        }
+        else {
+            console.log('Successful query');
+            console.log(rows);
+        }
+        programmeName = rows[0]['ProgrammeName'];
+        console.log("programmeName: " + programmeName);
+        res.send(programmeName);
+    });
 
-            else{
-                console.log('Successful Query');
-                console.log(rows);
-            }
-
-            programmeName = rows[0]['CourseName'];
-            console.log("programmeName " + programmeName);
-            res.send(programmeName);
-        })
 });
 
 app.listen(app.get('port'));
