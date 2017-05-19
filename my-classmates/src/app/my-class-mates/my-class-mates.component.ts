5ab00b39537fdecff667949b4190e4d72606af98
Brian McGowan - VM <S00165159@mail.itsligo.ie>
2017-05-19 10:17:43 +0100

    part 2 completed

--- my-classmates/src/app/my-class-mates/my-class-mates.component.ts
+++ my-classmates/src/app/my-class-mates/my-class-mates.component.ts
@@ -4,7 +4,28 @@ import { ClassMate } from '../class-mate';
 @Component({
   selector: 'app-my-class-mates',
   template: `
-    <app-class-mates [matesList]="mates"></app-class-mates>
+    <form class="ui large form segment">
+  <div class="field">
+    <label>First Name</label>
+    <input type="text" name="first-name" placeholder="First Name" #fName>
+  </div>
+  <div class="field">
+    <label>Last Name</label>
+    <input type="text" name="last-name" placeholder="Last Name" #lName>
+  </div>
+  <div class="field">
+    <label>Email</label>
+    <input type="text" name="email-name" placeholder="Email" #email>
+  </div>
+  <div class="field">
+    <label>Phone</label>
+    <input type="text" name="phone-name" placeholder="Phone" #phone>
+  </div>
+  <button class="ui positive button" (click)="addClassMate(fName, lName, email, phone)">
+  Save
+</button>
+</form>
+<app-class-mates [matesList]="mates"></app-class-mates>
   `,
   styleUrls: ['./my-class-mates.component.css']
 })
@@ -18,6 +39,14 @@ export class MyClassMatesComponent implements OnInit {
   }
 
   ngOnInit() {
+  }
+
+  addClassMate(fName: HTMLInputElement, lName: HTMLInputElement, email: HTMLInputElement, phone: HTMLInputElement): boolean {
+    console.log(`adding new dude ${fName.value} and lName  | ${email.value} | ${phone.value}`);
+    this.mates.push(new ClassMate(fName.value, lName.value, email.value, phone.value));
+    fName.value = lName.value = email.value = phone.value = "";
+    
+    return false;
   }
 
 }
